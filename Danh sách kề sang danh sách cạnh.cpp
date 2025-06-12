6b665bfd4d553f56e6b6eabfad28465c2fc8defc
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-06-12 09:43:36 +0700

    Create Danh sách kề sang danh sách cạnh.cpp

---
+++ Danh sách kề sang danh sách cạnh.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+vector<int> ke[1001];
+int main()
+{
+	int n;
+	cin >> n;
+	cin.ignore();
+	for (int i = 1; i <= n; i++) {
+		string s;
+		getline(cin, s);
+		stringstream ss(s);
+		int x;
+		while (ss >> x) {
+			ke[i].push_back(x);
+		}
+	}
+	vector<pair<int, int>> v;
+	for (int i = 1; i <= n; i++) {
+		for (auto x : ke[i]) {
+			if (i < x) v.push_back({ i,x });
+		}
+	}
+	sort(v.begin(), v.end());
+	for (auto x : v) {
+		cout << x.first << " " << x.second << endl;
+	}
+}
+
